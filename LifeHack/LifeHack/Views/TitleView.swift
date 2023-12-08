47b7688cebbbb4f6db14889e35ae8a9e9a05d4dd
Jimmy Lee <jimlee604@gmail.com>
2023-12-07 17:42:40 -0800

    clean up

--- LifeHack/LifeHack/Views/TitleView.swift
+++ LifeHack/LifeHack/Views/TitleView.swift
@@ -15,27 +15,27 @@ class TitleView: UIView {
   init(frame: CGRect, 
        presentLifeTrackerAction: Selector,
        presentingController: UIViewController) {
-    self.titleLabel = UILabel()
-    self.startButton = UIButton()
+    titleLabel = UILabel()
+    startButton = UIButton()
     super.init(frame: frame)
 
-    self.backgroundColor = .systemCyan
+    backgroundColor = .systemCyan
     let titleAttributes = [NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 50.0)!]
-    self.titleLabel.attributedText = NSAttributedString(string: "LifeHack", attributes:titleAttributes)
-    self.titleLabel.textAlignment = .center
+    titleLabel.attributedText = NSAttributedString(string: "LifeHack", attributes:titleAttributes)
+    titleLabel.textAlignment = .center
 
     let buttonTextAttributes = [
       NSAttributedString.Key.foregroundColor: UIColor.black,
       NSAttributedString.Key.font: UIFont(name: "MarkerFelt-Wide", size: 38.0)!
     ]
     let buttonAttributedText = NSAttributedString(string: "START", attributes: buttonTextAttributes)
-    self.startButton.setAttributedTitle(buttonAttributedText, for: .normal)
-    self.startButton.backgroundColor = .white
-    self.startButton.layer.cornerRadius = 12
-    self.startButton.addTarget(presentingController, action: presentLifeTrackerAction, for: .touchUpInside)
+    startButton.setAttributedTitle(buttonAttributedText, for: .normal)
+    startButton.backgroundColor = .white
+    startButton.layer.cornerRadius = 12
+    startButton.addTarget(presentingController, action: presentLifeTrackerAction, for: .touchUpInside)
 
-    self.addSubview(titleLabel)
-    self.addSubview(startButton)
+    addSubview(titleLabel)
+    addSubview(startButton)
   }
 
   required init?(coder aDecoder: NSCoder) {
@@ -43,20 +43,20 @@ class TitleView: UIView {
   }
 
   override func layoutSubviews() {
-    self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-      self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-      self.titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 200),
-      self.titleLabel.heightAnchor.constraint(equalToConstant: 80),
-      self.titleLabel.widthAnchor.constraint(equalToConstant: self.frame.width)
+      titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+      titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 200),
+      titleLabel.heightAnchor.constraint(equalToConstant: 80),
+      titleLabel.widthAnchor.constraint(equalToConstant: frame.width)
     ])
 
-    self.startButton.translatesAutoresizingMaskIntoConstraints = false
+    startButton.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-      self.startButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-      self.startButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -300),
-      self.startButton.heightAnchor.constraint(equalToConstant: 60),
-      self.startButton.widthAnchor.constraint(equalToConstant: 160)
+      startButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+      startButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -300),
+      startButton.heightAnchor.constraint(equalToConstant: 60),
+      startButton.widthAnchor.constraint(equalToConstant: 160)
     ])
   }
 }
